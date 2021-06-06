832f52891e239c552f04c28b0b734126cd3398f8
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-06 16:10:12 +0300

    finish lab01

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <h1>header</h1>;
+    return <h1>Gallary of Horns</h1>;
   }
 }
 
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -2,7 +2,13 @@ import { Component } from "react";
 
 class HornedBeasts extends Component {
   render() {
-    return <div>test</div>;
+    return (
+      <div className="beasts">
+        <h2>{this.props.title}</h2>
+        <p>{this.props.description}</p>
+        <img src={this.props.imageUrl} alt={this.props.title} />
+      </div>
+    );
   }
 }
 
