d2a78d5a145003d030a385b5977ac5a188721d7f
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-07 15:19:42 +0300

    add state to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,23 @@ import { Component } from "react";
 import Card from "react-bootstrap/Card";
 
 class Header extends Component {
+  state = {
+    text: "Gallary of Horns",
+  };
+
+  Change = () => {
+    this.setState({ text: "You clicked on the header" });
+  };
+
   render() {
     return (
-      <Card bg="dark" text="light">
-        Gallary of Horns
+      <Card
+        bg="dark"
+        text="light"
+        style={{ height: "5rem", textAlign: "center", fontSize: "30px" }}
+        onClick={this.Change}
+      >
+        {this.state.text}
       </Card>
     );
   }
